1b4e903db8fc1d151fde98d42b7d5211633b14f2
JG-Jeong <jo128g@naver.com>
2023-05-17 01:44:55 +0900

    router 구조 고침

--- dist/app.js
+++ dist/app.js
@@ -2,19 +2,38 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cats_route_1 = require("./cats/cats.route");
-var app = express();
 var port = 8000;
-app.use(function (req, res, next) {
-    console.log(req.rawHeaders[1]);
-    console.log("this is logging middleware.");
-    next();
-});
-app.use(express.json());
-app.use(cats_route_1.default);
-app.use(function (req, res, next) {
-    res.send({ error: "404 not found" });
-});
-app.listen(port, function () {
-    console.log("Example app  http://localhost:" + port);
-});
+var Server = (function () {
+    function Server() {
+        var app = express();
+        this.app = app;
+    }
+    Server.prototype.setRoute = function () {
+        this.app.use(cats_route_1.default);
+    };
+    Server.prototype.setMiddleware = function () {
+        this.app.use(function (req, res, next) {
+            console.log(req.rawHeaders[1]);
+            console.log("this is logging middleware.");
+            next();
+        });
+        this.app.use(express.json());
+        this.setRoute();
+        this.app.use(function (req, res, next) {
+            res.send({ error: "404 not found" });
+        });
+    };
+    Server.prototype.listen = function () {
+        this.setMiddleware();
+        this.app.listen(port, function () {
+            console.log("Example app  http://localhost:" + port);
+        });
+    };
+    return Server;
+}());
+function init() {
+    var server = new Server();
+    server.listen();
+}
+init();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,54 @@
 import * as express from "express";
 import catsRouter from "./cats/cats.route";
 
-import { error } from "console";
-
-const app: express.Express = express();
 const port: number = 8000;
 
-//* logging middleware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("this is logging middleware.");
-  next();
-});
+class Server {
+  public app: express.Application;
 
-//* json middleware
-app.use(express.json());
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-app.use(catsRouter);
+  // Route에 관련된 Middleware 작업
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-//* 404 middleware
-app.use((req, res, next) => {
-  res.send({ error: "404 not found" });
-});
+  // 미들웨어 작업
+  private setMiddleware() {
+    //* logging middleware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log("this is logging middleware.");
+      next();
+    });
 
-app.listen(port, () => {
-  console.log(`Example app  http://localhost:${port}`);
-});
+    //* json middleware
+    this.app.use(express.json());
+
+    this.setRoute();
+
+    //* 404 middleware
+    this.app.use((req, res, next) => {
+      res.send({ error: "404 not found" });
+    });
+  }
+
+  // public
+  public listen() {
+    //각각의 미들웨어를 불러와야 함
+    this.setMiddleware();
+    this.app.listen(port, () => {
+      console.log(`Example app  http://localhost:${port}`);
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
